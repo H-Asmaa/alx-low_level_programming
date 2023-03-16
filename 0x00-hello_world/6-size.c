25f95ca83896fcf425805ca5d9aa8661c7a04a0b
H-Asmaa <asmaehadar32@gmail.com>
2023-03-16 07:10:34 -0700

    Commiting i will kill u nuux

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char : %c byte(s)", sizeof(char));
+	printf("Size of a int : %d byte(s)", sizeof(int));
+	printf("Size of a long int : %d byte(s)", sizeof(long int));
+	printf("Size of a long long int : %d byte(s)", sizeof(long long int));
+	printf("Size of a float : %f byte(s)", sizeof(float));
+	return (0);
+}
