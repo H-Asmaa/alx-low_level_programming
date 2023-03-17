5189487b6d7ca96490ba7992bef3c968d7cf5bbd
H-Asmaa <asmaehadar32@gmail.com>
2023-03-18 00:53:34 +0100

    commiting numbers from 1 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,11 @@
  */
 int main(void)
 {
-	int i;
-	
-	for (i = 0; i <= 9; i++)
-	{
+    int i;
+    for (i = 0; i <= 9; i++)
+    {
         printf("%d", i);
-	}
-	putchar('\n');
-	return (0);
+    }
+    putchar('\n');
+    return (0);
 }
