19f37cec19f54f613542c8d2fa775e4537ecab01
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 14:35:01 -0700

    Commititing n rendomizer with modulo and checking if higher than 5 or less than 6 or equals to zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	printf("Last digit of %d is", n);
-	if (n == 0)
+	if (n%10 == 0)
 		printf(" %d and is 0\n", n % 10);
-	else if (n < 6)
+	else if (n%10 < 6)
 		printf(" %d and is less than 6 and not 0\n", n % 10);
 	else
 		printf(" %d and is greater than 5\n", n % 10);
