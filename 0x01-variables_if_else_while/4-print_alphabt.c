8fc00c223fef709cae01a3e4ff593fbc9ba75713
H-Asmaa <asmaehadar32@gmail.com>
2023-03-18 00:36:17 +0100

    Commiting the condition of q and e alphabet display using for loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char i;
+        for (i = 'a'; i <= 'z'; i++)
+        {
+            if (i != 'q' && i != 'e')
+            {
+                putchar(i);
+            }
+        }
+        putchar('\n');
+        return (0);
+}
