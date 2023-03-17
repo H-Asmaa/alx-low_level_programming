9defa772dd6d7b82ce2f1717f0346a6dec4cccb9
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 13:18:41 -0700

    Commiting randome n check if negative possitive or zero c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,9 +15,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n == 0)
-		printf("%d is negative\n", n);
-	else if (n < 0)
 		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
 		printf("%d is positive\n", n);
 	return (0);
