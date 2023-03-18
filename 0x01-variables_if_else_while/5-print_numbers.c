d16ed514a007a048421c97df8d0425b19b354078
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 17:33:09 -0700

    Commiting first

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,12 @@
  */
 int main(void)
 {
-    int i;
-    for (i = 0; i <= 9; i++)
-    {
-        printf("%d", i);
-    }
-    putchar('\n');
-    return (0);
+	int i
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
