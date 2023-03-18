8d016fd20446e100e3e2272d1565f9c5f8594d47
H-Asmaa <asmaehadar32@gmail.com>
2023-03-18 03:51:51 +0100

    GHJKL

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    for (i = 48; i <= 57; i++)
-    {
-        for (j = 48; j <= 57; j++)
-        {
-            putchar(i);
-            putchar(j);
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i < 98 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return 0;
 }
