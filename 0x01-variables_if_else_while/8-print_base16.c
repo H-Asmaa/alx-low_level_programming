11a73c48c6a8a289760ee87edf30e617143fdf80
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 17:54:46 -0700

    Commiting

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,16 @@
  */
 int main(void)
 {
-    char i;
-    for (i = '0'; i <= '9'; i++)
-    {
-        putchar(i);
-    }
-    for (i = 'a'; i <= 'f'; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
