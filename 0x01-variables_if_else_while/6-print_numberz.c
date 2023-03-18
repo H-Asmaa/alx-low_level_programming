0eba54728867baba56374752a4513813c7a3c5dd
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 17:44:21 -0700

    Commiting

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,12 @@
  */
 int main(void)
 {
-    int i;
-    for (i = 48; i <= 57; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
