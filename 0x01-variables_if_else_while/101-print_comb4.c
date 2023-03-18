4c3a7774ccd1fd1c9413d3c657107a6257ab3b99
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 19:41:04 -0700

    hbjk

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,26 +9,27 @@
  */
 int main(void)
 {
-    int i;       
-    int j;
-	int l;            
+	int i;
+	int j;
+	int l;
 
-    for (i = 48; i <= 57; i++)
-    {
-        for (j = i+1; j <= 57; j++)
-        {
-	        for (l = j+1; l <= 57; l++)
-	        {
-	            putchar(i);
-	            putchar(j);
-	            putchar(l);
-	            if (i < 55){
-		            putchar(',');
-		            putchar(' ');
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (l = j + 1; l <= 57; l++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(l);
+				if (i < 55)
+				{
+					putchar(',');
+					putchar(' ');
 				}
-	        }
-        }
-    }
-    putchar('\n');
-    return (0);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
