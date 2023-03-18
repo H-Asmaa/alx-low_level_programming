d6d7c062e85d5987802b6ec271d126959a5d419e
H-Asmaa <asmaehadar32@gmail.com>
2023-03-17 18:49:25 -0700

    commiting

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 57; i++)
 	{
-		for (j = i++; j <= 57; j++)
+		for (j = i++; j < 57; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != 57 || j != 57)
+			if (i < 56)
 			{
 				putchar(',');
 				putchar(' ');
