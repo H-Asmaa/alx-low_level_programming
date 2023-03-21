0481575808b91c889ba112e3fcfd9909ce52f2a2
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 07:07:15 +0100

    Commiting putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,14 @@
-#include <unistd.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int _putchar(char c)
+int _putchar(char c);
+int main()
 {
-	write(1, &c, 1);
-	return 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
