d60888eab7bb06e36c947f34b31327f67da12831
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 17:39:33 +0100

    Commiting 10 times alphabet function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-	char i;
 	int j;
+	char i;
+
 	for (j = 0; j < 10; j++)
 	{
-		print_alphabet();
+		for (i = 'a';i <= 'z';i++){
+			_putchar(i);
+		}
 		_putchar('\n');
 	}
 	_putchar('\n');
