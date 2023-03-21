952627e662056a1d074fdf4bb638b7bf385294df
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 18:15:48 +0100

    Commiting sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,25 @@
+#include "main.h"
+
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        printf("+");
-        return (1);
-    }
-    else if (n = 0)
-    {
-        printf("0");
-        return (0);
-    }
-    else
-    {
-        printf("-");
-        return (-1);
-    }
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n = 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
 }
