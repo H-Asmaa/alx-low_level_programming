a6915d2a399c6df3fc8d51a02134e4b039e38fec
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 23:57:12 +0100

    Commiting times table function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,34 +7,34 @@
  */
 void times_table(void)
 {
-    int i, j, n, res;
+	int i, j, n, res;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            n = i * j;
-            if (n > 9)
-            {
-                res = n / 10;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+			if (n > 9)
+			{
+				res = n / 10;
 
-                _putchar(res + '0');
-                _putchar(n % 10 + '0');
-            }
-            else
-            {
-                if (j != 0)
-                {
-                    _putchar(' ');
-                }
-                _putchar(n + '0');
-            }
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+				_putchar(res + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(n + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
