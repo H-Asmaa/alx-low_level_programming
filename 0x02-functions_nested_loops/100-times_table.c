2a00bf0ba76ca10053e8572f8e5ee3a2695e0fd8
H-Asmaa <asmaehadar32@gmail.com>
2023-03-22 00:43:39 +0100

    Commiting 98 function

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - check the code.
+ * @n: variable
+ * Description
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98 && n <= 98; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	for (i = n; i >= 98 && n > 98; i--)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
