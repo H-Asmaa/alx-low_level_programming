934b19ff6629bdf66e586fdc80ae964ee447c91d
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 07:59:09 +0100

    Commiting alphabet 10 time

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+int _putchar(char c);
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
