0ef8e88aa06372f756756d837df5de5a05064f1e
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 23:54:30 +0100

    Commiting times table function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,10 @@ void times_table(void)
             }
             else
             {
-                _putchar(' ');
+                if (j != 0)
+                {
+                    _putchar(' ');
+                }
                 _putchar(n + '0');
             }
             if (j < 9)
