8f7533cf2388efc8fa824ba0daa2b9c70c2e9b87
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 06:31:29 +0100

    Commiting writer function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int _putchar(void){
-	printf("_putchar\n");
-	return (0);
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return 0;
 }
