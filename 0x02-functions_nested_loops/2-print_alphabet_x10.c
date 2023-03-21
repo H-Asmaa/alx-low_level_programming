f7943785762d82b4355880b3a491b5bbff942748
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 16:57:19 +0100

    Commiting print_alpha*10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,7 @@ void print_alphabet_x10(void)
 	int j;
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
+		print_alphabet();
 		_putchar('\n');
 	}
 	_putchar('\n');
