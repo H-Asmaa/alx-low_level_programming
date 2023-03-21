a4e4df7670f8eb2382bba47edcfcd77401ce9edb
H-Asmaa <asmaehadar32@gmail.com>
2023-03-21 17:40:50 +0100

    commiting alpha time 10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,10 @@ void print_alphabet_x10(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 'a';i <= 'z';i++){
+		for (i = 'a'; i <= 'z'; i++)
+		{
 			_putchar(i);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
