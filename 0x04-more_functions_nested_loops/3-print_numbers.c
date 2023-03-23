1fa0be223984d51d5c2ff699e840d9731aa182bb
H-Asmaa <asmaehadar32@gmail.com>
2023-03-23 23:24:41 +0100

    updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * mul - check the code.
+ * print_numbers - check the code.
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
