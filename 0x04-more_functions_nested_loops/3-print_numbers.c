ce0ed2933c7028dc5de0a37a00e818fc644ebed4
H-Asmaa <asmaehadar32@gmail.com>
2023-03-23 23:22:37 +0100

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check the code.
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return 0;
+}
