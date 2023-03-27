42e1782941b4fec0077886333ae7ecd31400a4df
H-Asmaa <asmaehadar32@gmail.com>
2023-03-27 23:56:12 +0000

    updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,24 @@
 #include "main.h"
 
 /**
- * reset_to_98 - check the code
- * @n: variable
+ * print_rev - check the code
+ * @s: pointer
  * Return: void.
  */
-void reset_to_98(int *n)
+void print_rev(char *s)
 {
+	int i;
+	int j;
+	int count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (j = count; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }
