4305a50ddda6325a6e2275459eceebe1cf3b7994
H-Asmaa <asmaehadar32@gmail.com>
2023-03-28 01:41:56 +0000

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,33 @@
 #include "main.h"
 
 /**
- * reset_to_98 - check the code
- * @n: variable
+ * puts_half - check the code
+ * @str: variable
  * Return: void.
  */
-void reset_to_98(int *n)
+void puts_half(char *str)
 {
+	int count;
+	int i;
+
+	count = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; i <= count; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = count - 1 / 2; i <= count; i--)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
