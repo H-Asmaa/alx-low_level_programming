2e831795144eb7b771c030fa30a5f0f7b9c4e36b
H-Asmaa <asmaehadar32@gmail.com>
2023-03-28 00:58:07 +0000

    updated

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _strlen - check the code
  * @s: pointer
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
 /**
- * reset_to_98 - check the code
- * @n: variable
- * Return: void.
+ * rev_string - reverses a string
+ * @s: string to reverse
  */
-void reset_to_98(int *n)
+void rev_string(char *s)
 {
+	int i, j, count;
+	char tmp;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	for (i = 0; i < count / 2; i++)
+	{
+		j = count - i - 1;
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+	}
 }
