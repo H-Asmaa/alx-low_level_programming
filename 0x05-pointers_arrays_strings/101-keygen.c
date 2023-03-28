6078e8e459dd76a76903241603e5b9f02f44bf8b
H-Asmaa <asmaehadar32@gmail.com>
2023-03-28 02:40:14 +0000

    updated

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,23 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * reset_to_98 - check the code
- * @n: variable
  * Return: void.
  */
-void reset_to_98(int *n)
+int main(void)
 {
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
