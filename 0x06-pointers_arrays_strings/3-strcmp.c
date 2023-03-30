884c6e93227b02f3a7605d6ecf4c2f8d76f69956
H-Asmaa <asmaehadar32@gmail.com>
2023-03-30 16:23:10 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - check the code
+ * @s1: pointer destination
+ * @s2: pointer source
+ * Return: void.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
