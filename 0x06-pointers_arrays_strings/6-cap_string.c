466cd98d97094c34030826ca9241c6a7026c424b
H-Asmaa <asmaehadar32@gmail.com>
2023-03-30 23:59:15 +0000

    updated

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - check the code
+ * @str: pointer
+ * Return: void.
+ */
+char *cap_string(char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+            || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+            || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
+            || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+            || str[i - 1] == '}')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+    return str;
+}
