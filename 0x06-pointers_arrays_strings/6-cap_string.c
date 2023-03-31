6b528e31a5ee750f4af0cc312e5752929536d936
H-Asmaa <asmaehadar32@gmail.com>
2023-03-31 00:38:22 +0000

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * cap_string - check the code
+ * string_toupper - check the code
  * @str: pointer
- * Return: char.
+ * Return: void.
  */
 char *cap_string(char *str)
 {
-	int i;
+    int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - ('a' - 'A');
-			}
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			str[i] = str[i] + ('a' - 'A');
-		}
-	}
-	return (str);
+    for (i = 1; str[i] != '\0'; i++)
+    {
+        /* check for word separators */
+        if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+            || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+            || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
+            || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+            || str[i - 1] == '}')
+        {
+            /* check if current character is lowercase */
+            if (str[i] >= 'a' && str[i] <= 'z')
+                str[i] -= ('a' - 'A');
+        }
+    }
+
+    return (str);
 }
