c8090f99f9a89eee3d868fefed25e648edeeae06
H-Asmaa <asmaehadar32@gmail.com>
2023-03-31 00:00:46 +0000

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,17 @@
  */
 char *cap_string(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-            || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
-            || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
-            || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-            || str[i - 1] == '}')
-        {
-            str[i] = str[i] - 32;
-        }
-    }
-    return str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - ('a' - 'A');
+			}
+		}
+	}
+	return str;
 }
