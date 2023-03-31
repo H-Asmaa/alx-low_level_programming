4bb91c5a796105d39016242bd77d3aeb05213cc9
H-Asmaa <asmaehadar32@gmail.com>
2023-03-31 00:25:46 +0000

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * string_toupper - check the code
+ * cap_string - check the code
  * @str: pointer
- * Return: void.
+ * Return: char.
  */
 char *cap_string(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-        {
-            if (str[i] >= 'a' && str[i] <= 'z')
-            {
-                str[i] = str[i] - ('a' - 'A');
-            }
-        }
-        else if (str[i] >= 'A' && str[i] <= 'Z')
-        {
-            str[i] = str[i] + ('a' - 'A');
-        }
-    }
-    return str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - ('a' - 'A');
+			}
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + ('a' - 'A');
+		}
+	}
+	return (str);
 }
