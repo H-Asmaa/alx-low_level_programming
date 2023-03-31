e52392c7a79292645a424d1463dda58ab06c427d
H-Asmaa <asmaehadar32@gmail.com>
2023-03-31 00:45:41 +0000

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,22 +7,17 @@
  */
 char *cap_string(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 1; str[i] != '\0'; i++)
-    {
-        /* check for word separators */
-        if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
-            || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
-            || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
-            || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
-            || str[i - 1] == '}')
-        {
-            /* check if current character is lowercase */
-            if (str[i] >= 'a' && str[i] <= 'z')
-                str[i] -= ('a' - 'A');
-        }
-    }
-
-    return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == ',' || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '\n')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
 }
