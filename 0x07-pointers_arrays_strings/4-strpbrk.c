319f1e9805f87dd49ef57f1425d374b12ffa3143
H-Asmaa <asmaehadar32@gmail.com>
2023-04-03 23:56:40 +0000

    Commiting strpbrk a function that returns the string beguining with the first occurence found from another string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - check the code
+ * @s: pointer.
+ * @accept: pointer.
+ * Return: char.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	if (accept[i] == '\0')
+		return (s);
+	return ('\0');
+}
