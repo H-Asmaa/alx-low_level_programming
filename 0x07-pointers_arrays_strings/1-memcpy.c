4804a1b47af17b54e7feae0d3a0562d072d2df4d
H-Asmaa <asmaehadar32@gmail.com>
2023-04-03 15:28:53 +0000

    Commiting memcoy function that coppies the content of a an array into another array untill it reaches the n th element

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - check the code
+ * @dest: pointer.
+ * @src: pointer.
+ * @n: variable.
+ * Return: void.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
