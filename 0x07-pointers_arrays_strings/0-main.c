a61cfc7351a1897a891a92cf104303fed066f111
H-Asmaa <asmaehadar32@gmail.com>
2023-04-03 15:15:20 +0000

    Commiting the memset a function that copies a char inside of an array n times

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -6,7 +6,7 @@
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
- * Return: Nothing.
+ * Return: Nothing
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
