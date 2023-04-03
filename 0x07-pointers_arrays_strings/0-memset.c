37284b3c36476e32e29f75fc7b304cb0f30d3185
H-Asmaa <asmaehadar32@gmail.com>
2023-04-03 13:38:11 +0000

    updated

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - check the code
+ * @s: pointer
+ * @b: variable
+ * @n: variable
+ * Return: void.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
