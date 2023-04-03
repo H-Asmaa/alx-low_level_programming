00ed5ca5f4c192d8f24959e1d40089bfd97064ba
H-Asmaa <asmaehadar32@gmail.com>
2023-04-03 15:50:58 +0000

    Commiting strchr a function that finds a given character and returns it with the characters left in the string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - check the code
+ * @s: pointer.
+ * @c: variable.
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s = (s + i);
+			return (s);
+		}
+	}
+	return (s);
+}
