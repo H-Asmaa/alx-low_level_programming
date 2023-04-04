3bdb972bdfcbaa34c1b688e2882d360413448e34
H-Asmaa <asmaehadar32@gmail.com>
2023-04-04 02:48:08 +0000

    Commiting sum of diagnoles left and right function

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_diagsums - check the code
+ * @s: pointer to a pointer.
+ * @to: pointer.
+ * Return: char.
+ */
+void set_string(char **s, char *to)
+{
+
+	**s = to;	
+}
