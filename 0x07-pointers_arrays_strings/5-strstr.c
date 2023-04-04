2381ccbc53eef538ee8620d204bf80f44dcd7e88
H-Asmaa <asmaehadar32@gmail.com>
2023-04-04 01:51:21 +0000

    Commiting strstr function that returns the substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,13 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j + 1] == '\0')
-			{
-				return (&haystack[i]);
-			}
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
 	}
-
 	return (NULL);
 }
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strstr - check the code
+ * @haystack: pointer.
+ * @needle: pointer.
+ * Return: char.
+ */
+void print_chessboard(char (*a)[8])
+{
+}
