4bc0269b258795be4031c53e0ac2f3667330b296
H-Asmaa <asmaehadar32@gmail.com>
2023-04-04 01:05:31 +0000

    Commiting strstr a function that search with a string trough a string and returns the first occurence

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (j = 0; needle[j] != '\0'; j++)
 	{
-		for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++)
+		if (needle[j] == '\0')
 		{
-			if (needle[j + 1] == '\0')
+			break;
+		}
+		for (i = 0; haystack[i] != '\0'; i++)
+		{
+			if (haystack[i] == needle[j])
 			{
-				return (&haystack[i]);
+				return (haystack + i);
 			}
 		}
 	}
-
-	return (NULL);
+	return ('\0');
 }
