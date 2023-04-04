4520a217933cee357a75f198156fb5be65407511
H-Asmaa <asmaehadar32@gmail.com>
2023-04-04 01:57:22 +0000

    Commiting strstr function that returns the substring

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * _strstr - check the code
- * @haystack: pointer.
- * @needle: pointer.
+ * print_chessboard - check the code
+ * @a: pointer.
  * Return: char.
  */
 void print_chessboard(char (*a)[8])
 {
+	int i;
+	int j;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	}
 }
