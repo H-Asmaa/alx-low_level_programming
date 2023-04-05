1d63cae4048ae5581f1455cb54dad91456829171
H-Asmaa <asmaehadar32@gmail.com>
2023-04-05 17:08:33 +0000

    Commiting recursive to display a string using _putchar function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: pointer.
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(s + 1);
+	}else{
+		_putchar('\n');
+	}
+}
