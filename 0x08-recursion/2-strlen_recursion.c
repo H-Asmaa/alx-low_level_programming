f68d73f00e088808fd9b444a3abfc7623149498d
H-Asmaa <asmaehadar32@gmail.com>
2023-04-05 18:37:28 +0000

    Commiting recursion to return the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - short cut { return (*s != '\0' ? _strlen_recursion(s + 1) + 1 : 0); }
+ * @s: pointer.
+ * Return: void.
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s != '\0')
+		len = _strlen_recursion(s + 1) + 1;
+	else
+		len = 0;
+	return (len);
+}
