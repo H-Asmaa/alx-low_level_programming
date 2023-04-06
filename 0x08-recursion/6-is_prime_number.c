903e80e37d93bb8be6e27263b1db8064c2d945e2
H-Asmaa <asmaehadar32@gmail.com>
2023-04-06 02:29:10 +0000

    Commiting recursion that checks if a number is prime or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,14 @@
 int is_prime_helper(int n, int i)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 
 	if (i == n)
-		return 1;
+		return (1);
 
 	if (n % i == 0)
-		return 0;
-
-	return is_prime_helper(n, i + 1);
+		return (0);
+	return (is_prime_helper(n, i + 1));
 }
 /**
  * is_prime_number - check the code
@@ -26,5 +25,5 @@ int is_prime_helper(int n, int i)
  */
 int is_prime_number(int n)
 {
-	return is_prime_helper(n, 2);
+	return (is_prime_helper(n, 2));
 }
