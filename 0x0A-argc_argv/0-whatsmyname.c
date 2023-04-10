ea7e59f597e23a2f985b20a5e9c79c7ea8a90618
H-Asmaa <asmaehadar32@gmail.com>
2023-04-10 21:23:40 +0000

    Commiting a function that printf the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return 0;
+}
