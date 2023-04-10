bab3a7229166f2fa70b6cddfff465cd89b8ec040
H-Asmaa <asmaehadar32@gmail.com>
2023-04-10 21:47:10 +0000

    Commiting program that prints the content of argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argv: parameter
+ *@argc: parameter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
