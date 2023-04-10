7be0a00c9db83af8e9b1e9f79c1e062153168ab9
H-Asmaa <asmaehadar32@gmail.com>
2023-04-10 21:34:35 +0000

    Commiting a program that display the number of arguments given

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argv: parameter
+ *@argc: parameter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,5 @@
 #define MYLIBRARY_H
 
 int _putchar(char c);
-int main(int argc, char *argv[]);
 
 #endif
