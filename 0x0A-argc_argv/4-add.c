a2f9bdaacb852aea55cd06e5bae95b7b8317685f
H-Asmaa <asmaehadar32@gmail.com>
2023-04-10 22:49:58 +0000

    Commiting program that does sum of numbers given in params

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argv: parameter
+ * @argc: parameter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int tmp;
+	int sum = 0;
+	if (argc > 1)
+	{
+		tmp = argc;
+		while (tmp > 1)
+		{
+			sum += atoi(argv[tmp - 1]);
+			tmp--;
+		}
+		printf("%d", sum);
+		printf("\n");
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
