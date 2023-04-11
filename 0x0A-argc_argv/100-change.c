e68642437a63f16feea692f0a193835f69b7cb94
H-Asmaa <asmaehadar32@gmail.com>
2023-04-11 01:11:52 +0000

    Commiting a program that manages coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -30,7 +30,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-
 	while (tmp > 0)
 	{
 		if (tmp >= 25)
@@ -48,36 +47,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", counter);
 	return (0);
 }
-/*
-int main(int argc, char *argv[])
-{
-	int cents;
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins;
-	int i;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return 1;
-	}
-
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return 0;
-	}
-
-	num_coins = 0;
-
-	for (i = 0; i < 5 && cents > 0; i++)
-	{
-		num_coins += cents / coins[i];
-		cents %= coins[i];
-	}
-
-	printf("%d\n", num_coins);
-	return 0;
-}*/
