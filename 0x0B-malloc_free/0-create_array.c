f2ed0371538e1ad82440c57db47851444908945a
H-Asmaa <asmaehadar32@gmail.com>
2023-04-11 21:25:45 +0000

    Commiting function that allocates memory for an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - check the code.
+ * @size: variable
+ * @c: variable
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size <= 0)
+		return ('\0');
+	p = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MYLIBRARY_H
+#define MYLIBRARY_H
+
+char *create_array(unsigned int size, char c);
+
+#endif
