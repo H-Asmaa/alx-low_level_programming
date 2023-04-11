2902f46b8afbfb64fdade3e5bc606b7ff008d000
H-Asmaa <asmaehadar32@gmail.com>
2023-04-11 00:50:45 +0000

    Commiting a program that manages coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argv: parameter
+ * @argc: parameter
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int tmp;
+	int counter;
+
+	tmp = atoi(argv[1]);
+	counter = 0;
+
+	if (argc != 2 || !isdigit(*argv[1]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (tmp <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (tmp > 0)
+	{
+		if (tmp >= 25)
+			tmp -= 25;
+		else if (tmp >= 10)
+			tmp -= 10;
+		else if (tmp >= 5)
+			tmp -= 5;
+		else if (tmp >= 2)
+			tmp -= 2;
+		else if (tmp >= 1)
+			tmp -= 1;
+		counter++;
+	}
+	printf("%d\n", counter);
+	return (0);
+}
