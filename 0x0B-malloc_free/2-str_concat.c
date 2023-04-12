86426bb262168a95478ca659ebafb10ba28fed4e
H-Asmaa <asmaehadar32@gmail.com>
2023-04-12 00:46:04 +0000

    Commiting a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,11 +19,13 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
-	if (s1 != NULL){
+	if (s1 != NULL)
+	{
 		while (s1[len1] != '\0')
 			len1++;
 	}
-	if (s2 != NULL){
+	if (s2 != NULL)
+	{
 		while (s2[len2] != '\0')
 			len2++;
 	}
