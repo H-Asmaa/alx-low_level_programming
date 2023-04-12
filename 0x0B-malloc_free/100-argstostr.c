f82c6cc19dc00fb846c7c95e14a63fc05aa8ee53
H-Asmaa <asmaehadar32@gmail.com>
2023-04-12 02:55:03 +0000

    Commiting  function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,35 +8,34 @@
  * @av: variable
  * Return: Always 0.
  */
+#include <stdlib.h>
+#include <string.h>
+
 char *argstostr(int ac, char **av)
 {
-	int i;
-	int j;
-	int len;
-	char *p;
+    int i, j;
+    char *p;
+    int len = 0;
 
-	len = 0;
+    if (ac == 0 || av == NULL)
+        return (NULL);
+    for (i = 0; i < ac; i++)
+    {
+        len += strlen(av[i]);
+        len++;
+    }
+    p = malloc(sizeof(char) * (len + 1));
+    if (p == NULL)
+        return (NULL);
+    len = 0;
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+            p[len++] = av[i][j];
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+        p[len++] = '\n';
+    }
+    p[len] = '\0';
 
-	for (i = 0; i < ac; i++)
-	{
-		while (av[i][len] != '\0')
-			len++;
-		len++;
-	}
-	p = malloc(sizeof(char) * len);
-	if (p == NULL)
-		return (NULL);
-	len = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			p[len++] = av[i][j];
-
-		p[len++] = '\n';
-	}
-	p[len] = '\0';
-	return (p);
+    return (p);
 }
