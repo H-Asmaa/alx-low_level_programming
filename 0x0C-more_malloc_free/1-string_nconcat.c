7adf1f7a1c2ba646eabd45ef381c302696da7248
H-Asmaa <asmaehadar32@gmail.com>
2023-04-13 13:45:26 +0000

    Commiting a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,25 +16,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int j;
 	unsigned int q;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (l = 0; s2[l] != '\0'; l++)
-		;
-	if (n >= l)
-		n = l;
-	p = (char *)malloc((i + n) * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	j = 0;
-	while (s1[j] != '\0')
+	if (s1 != NULL && s2 != NULL)
 	{
-		p[j] = s1[j];
-		j++;
+		for (i = 0; s1[i] != '\0'; i++)
+			;
+		for (l = 0; s2[l] != '\0'; l++)
+			;
+		if (n >= l)
+			n = l;
+		p = (char *)malloc((i + n) * sizeof(char));
+		if (p == NULL)
+			return (NULL);
+		j = 0;
+		while (s1[j] != '\0')
+		{
+			p[j] = s1[j];
+			j++;
+		}
+		for (q = 0; q < n; q++)
+		{
+			p[j] = s2[q];
+			j++;
+		}
+		return (p);
 	}
-	for (q = 0; q < n; q++)
-	{
-		p[j] = s2[q];
-		j++;
-	}
-	return (p);
+	return (NULL);
 }
