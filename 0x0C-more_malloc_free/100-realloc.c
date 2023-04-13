011430d2b9946edad83538f29d3d453c973f5dff
H-Asmaa <asmaehadar32@gmail.com>
2023-04-13 23:05:59 +0000

    Committing a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,6 +25,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	else
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size > old_size)
 	{
 		tmp = new_size + old_size;
