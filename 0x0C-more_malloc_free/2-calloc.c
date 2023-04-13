f6d7b556d7e74c344450676dc57e11143b6eb0bb
H-Asmaa <asmaehadar32@gmail.com>
2023-04-13 20:00:12 +0000

    Commiting a function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = (void *)malloc(size * nmemb);
+	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
