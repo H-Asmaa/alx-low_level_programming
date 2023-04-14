f464d98267734034aa242bfc90795bec3f11ac2e
H-Asmaa <asmaehadar32@gmail.com>
2023-04-14 00:04:33 +0000

    Committing a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,38 +11,38 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *p;
-    unsigned int i;
-    unsigned int tmp1;
+	char *p;
+	unsigned int i;
+	unsigned int tmp1;
 
-    if (ptr == NULL)
-        return ((void *)malloc(new_size));
-    if (new_size == old_size)
-        return (ptr);
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-    if (new_size > old_size)
-        tmp1 = old_size;
-    else
-        tmp1 = new_size;
-    p = malloc(tmp1);
-    if (p == NULL)
-        return (NULL);
-    for (i = 0; i < tmp1; i++)
-    {
-        ((char *)p)[i] = ((char *)ptr)[i];
-    }
-    free(ptr);
-    ptr = (void *)malloc(new_size);
-    if (ptr == NULL)
-        return (NULL);
-    for (i = 0; i < tmp1; i++)
-    {
-        ((char *)ptr)[i] = ((char *)p)[i];
-    }
-    free(p);
-    return (ptr);
+	if (ptr == NULL)
+		return ((void *)malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+		tmp1 = old_size;
+	else
+		tmp1 = new_size;
+	p = malloc(tmp1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < tmp1; i++)
+	{
+		((char *)p)[i] = ((char *)ptr)[i];
+	}
+	free(ptr);
+	ptr = (void *)malloc(new_size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < tmp1; i++)
+	{
+		((char *)ptr)[i] = ((char *)p)[i];
+	}
+	free(p);
+	return (ptr);
 }
