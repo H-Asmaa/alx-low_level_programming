e1773f4039fb7080a6fef596d8b93274eb454586
H-Asmaa <asmaehadar32@gmail.com>
2023-04-17 21:39:52 +0000

    Committing a function that creates a new dog.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - check the code.
+ * @name: pointer
+ * @age: variable
+ * @owner: pointer
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	p = malloc(sizeof(dog_t));
+	if (p != NULL)
+	{
+		(*p).name = name;
+		(*p).age = age;
+		(*p).owner = owner;
+	}
+	return (p);
+}
