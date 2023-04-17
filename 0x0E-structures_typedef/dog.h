9f9db13321e39d7fa0e4b0621bb97c707f11daac
H-Asmaa <asmaehadar32@gmail.com>
2023-04-17 19:51:29 +0000

    Committing new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_h
+#define DOG_h
+/**
+ * struct dog - strucutes
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: A structure dog with the name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
