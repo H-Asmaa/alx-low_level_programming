01d32775a1da1b5767a7e18ca774ced937262be1
H-Asmaa <asmaehadar32@gmail.com>
2023-04-17 21:12:31 +0000

    Committing a function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - check the code.
+ * @d: pointer
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	if (d->age != 0)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	if (d->owner != 0)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
