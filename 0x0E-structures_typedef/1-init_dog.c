f62e3ba19af0d4b6ed137b3f8838df54adac19d1
H-Asmaa <asmaehadar32@gmail.com>
2023-04-17 20:07:41 +0000

    Committing a function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - check the code.
+ * @d: pointer
+ * @name: variable
+ * @age: variable
+ * @owner: variable
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
