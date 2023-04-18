a77a01d3a9c80208acd46d2baecfe65a4fd596a3
H-Asmaa <asmaehadar32@gmail.com>
2023-04-18 01:09:39 +0000

    Committing a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,23 +13,23 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
+	dog_t *new_dog;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-    new_dog->name = malloc(strlen(name) + 1);
-    if (new_dog->name == NULL)
-    {
-        return (NULL);
-    }
-    strcpy(new_dog->name, name);
-    new_dog->age = age;
-    new_dog->owner = malloc(strlen(owner) + 1);
-    if (new_dog->owner == NULL)
-    {
-        return (NULL);
-    }
-    strcpy(new_dog->owner, owner);
-    return (new_dog);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = malloc(strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = malloc(strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
