4d14246df633aa9dfd6bc2c06ab0993009bb45ed
H-Asmaa <asmaehadar32@gmail.com>
2023-04-19 18:17:26 +0000

    Committing a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - check the code.
  * @name: pointer
@@ -7,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL && name != NULL)
+		f(name);
 }
