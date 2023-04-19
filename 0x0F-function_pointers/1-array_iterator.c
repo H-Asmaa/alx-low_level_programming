a5942e5c4ff3a9058ab5df6d7465814923b8de6d
H-Asmaa <asmaehadar32@gmail.com>
2023-04-19 18:45:10 +0000

    Committing a function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	if (array != NULL && size != 0)
+	if (action != NULL && array != 0)
 	{
 		for (i = 0; i < size; i++)
 		{
