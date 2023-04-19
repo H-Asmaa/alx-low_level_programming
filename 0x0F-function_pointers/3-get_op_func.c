bb69609a2d037481e506bbb83ef220eb4e6e9b2d
H-Asmaa <asmaehadar32@gmail.com>
2023-04-19 22:17:00 +0000

    Committing a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * get_op_func - check the code.
+ * @s: the operator passed as argument to the program
+ * Return: pointer.
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ * @argc: parametre
+ * @argv: parametre
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	char *op;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+
+	if (b == 0 && (*op == '/' || *op == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	func = get_op_func(op);
+
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(a, b));
+
+	return (0);
+}
