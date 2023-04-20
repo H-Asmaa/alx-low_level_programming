ae801c8fd2049095a29fc46ebee3d80124b69fd4
H-Asmaa <asmaehadar32@gmail.com>
2023-04-20 00:18:56 +0000

    Committing a program that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: parametre
+ * @argv: parametre
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int tmp;
+	char *p;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	tmp = atoi(argv[1]);
+	if (tmp <= 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	p = (char *)main;
+	i = 0;
+	while (i < tmp)
+	{
+		printf("%02x ", p[i] & 0xff);
+		i++;
+	}
+	return (0);
+}
