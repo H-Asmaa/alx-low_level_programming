62f7b8f1b112517b006abbd958bdb9ebff32a264
H-Asmaa <asmaehadar32@gmail.com>
2023-04-20 19:52:39 +0000

    Committing a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - check the code.
+ * @format: A list of types of arguments passed to the function
+ * Return: void.
+ */
+void print_all(const char *const format, ...)
+{
+	int i;
+	char *separator = "";
+	char *ps;
+	va_list arg;
+
+	i = 0;
+	va_start(arg, format);
+
+	while (format[i] != '\0')
+	{
+		separator = "";
+		switch (format[i])
+		{
+		case 'i':
+			printf("%d", va_arg(arg, int));
+			separator = ", ";
+			break;
+		case 'c':
+			printf("%c", va_arg(arg, int));
+			separator = ", ";
+			break;
+		case 'f':
+			printf("%f", va_arg(arg, double));
+			separator = ", ";
+			break;
+		case 's':
+			ps = va_arg(arg, char *);
+			if (ps == NULL || ps[0] == '\0')
+				printf("(nil)");
+			else
+				printf("%s", ps);
+			separator = ", ";
+			break;
+		}
+		i++;
+		if (format[i] != '\0')
+			printf("%s", separator);
+	}
+	va_end(arg);
+	printf("\n");
+}
