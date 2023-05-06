21cda5aecdad2a1194e3d80c7cbdec4c56327c21
H-Asmaa <asmaehadar32@gmail.com>
2023-05-06 15:14:53 +0100

    Committing a function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,14 +8,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int tmp;
-
-	tmp = 1;
-	tmp = tmp << index;
-	if ((n & tmp) != 0)
-		return (1);
-	else if ((n & tmp) != 1)
-		return (0);
-	else
+	unsigned long int mask = 1;
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
+
+	mask = mask << index;
+
+	if ((n & mask) == mask)
+		return 1;
+	else
+		return 0;
 }
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,7 +16,7 @@ int main(void)
 {
     int n;
 
-    n = get_bit(1024, -1);
+    n = get_bit(1024, 1);
     printf("%d\n", n);
     n = get_bit(98, 1);
     printf("%d\n", n);
