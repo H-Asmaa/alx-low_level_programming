f7f945a76de94c32cdd603e3ab7cb8386969f64a
H-Asmaa <asmaehadar32@gmail.com>
2023-05-07 16:59:11 +0100

    Committing a function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,13 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
-
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	mask = mask << index;
 
 	if ((n & mask) == mask)
-		return (1);
+		return 1;
 	else
-		return (0);
+		return 0;
 }
