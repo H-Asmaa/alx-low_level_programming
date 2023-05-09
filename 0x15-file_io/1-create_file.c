e2b6ef8828ca9a804fd1d9343cc5f1b8a8bf558a
H-Asmaa <asmaehadar32@gmail.com>
2023-05-09 02:22:27 +0100

    Committing a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
 	count = 0;
-	while (text_content[count] != '\0')
+	while (text_content != NULL && text_content[count] != '\0')
 	{
 		count++;
 	}
