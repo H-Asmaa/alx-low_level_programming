b0259632e5567d01682a84a5f7c86ddde52b6858
H-Asmaa <asmaehadar32@gmail.com>
2023-05-10 00:15:45 +0100

    Committing a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,43 +1,52 @@
-#include "main.h"
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argv: parameter
+ *@argc: parameter
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	int count;
-	char buffer[150];
-	int f;
-	int s;
-	int tmp;
+	int f_from, f_to, write_f, read_f;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
-		write(STDOUT_FILENO, "Usage: cp file_from file_to\n", 28);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (access(argv[1], F_OK) != 0)
+	f_from = open(argv[1], O_RDONLY);
+	if (f_from == -1)
 	{
-		count = sprintf(buffer, "Error: Can't read from file %s\n", argv[1]);
-		write(STDOUT_FILENO, buffer, count);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (access(argv[2], F_OK) == 0)
+	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (f_to == -1)
 	{
-		f = open(argv[2], O_WRONLY | O_TRUNC, 644);
-		if (f == -1)
-			return (-1);
-		s = open(argv[1], O_RDONLY, 644);
-		if (s == -1)
-			return (-1);
-		/*Copying the content of file from to file to (STILL IN PROCESS)*/
-		tmp = read(argv[1], buffer, sizeof(150));
-
-		close(f);
-	}
-	else
-	{
-		count = sprintf(buffer, "Error: Can't write to %s\n", argv[2]);
-		write(STDOUT_FILENO, buffer, count);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
+	}
+
+	while ((read_f = read(f_from, buffer, 1024)) > 0)
+	{
+		write_f = write(f_to, buffer, read_f);
+		if (write_f != read_f)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	if (close(f_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from);
+		exit(100);
+	}
+	if (close(f_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
+		exit(100);
 	}
 	return (0);
 }
