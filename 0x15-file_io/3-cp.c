350680a83ab79428ad9562659113cd8804560ef0
H-Asmaa <asmaehadar32@gmail.com>
2023-05-10 00:42:08 +0100

    Committing a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
