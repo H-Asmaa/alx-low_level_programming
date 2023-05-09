5f99e76dbee7e2630faab431a68be70a9ec180bb
H-Asmaa <asmaehadar32@gmail.com>
2023-05-09 02:04:07 +0100

    Committing a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - check the code.
+ * @filename: a pointer
+ * @text_content: a pointer
+ * Return: return.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	int tmp;
+	int count;
+
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
+	count = 0;
+	while (text_content[count] != '\0')
+	{
+		count++;
+	}
+	tmp = write(f, text_content, count);
+	if (tmp == -1)
+		return (-1);
+	close(f);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,4 +7,5 @@
 
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
