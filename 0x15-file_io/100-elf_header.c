fb5fb9b1f827b932f20c2bceced7bb368033c503
H-Asmaa <asmaehadar32@gmail.com>
2023-05-12 23:28:45 +0100

    Committing a program that checks if a file of type ELF and displays it's infos

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,9 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 /**
  * main - check the code
  * @argv: parameter
  * @argc: parameter
  * Return: Always 0.
  */
-int main()
+int main(int argc, char *argv[])
 {
+	int f;
+	char *read_f = malloc(32);
+	char *magic_num = "\x7f"
+					  "ELF";
+
+	if (argc != 2)
+	{
+		perror("arg");
+		return (1);
+	}
+	f = open(argv[1], O_RDONLY);
+	if (f == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	if (read(f, read_f, 32) == -1)
+	{
+		perror("read");
+		close(f);
+		return (1);
+	}
+	if (strcmp(read_f, magic_num) == 0)
+		printf("The file is of type ELF\n");
+	else
+		printf("The file is not of type ELF\n");
+	close(f);
+	free(read_f);
+	return (0);
 }
