74d9781adbaf5fe4e26be86e29a41fe4bb318aa1
H-Asmaa <asmaehadar32@gmail.com>
2023-05-29 19:00:54 +0100

    Committing the function for checker testing only

--- 0x13-more_singly_linked_lists/101-main.c
+++ 0x13-more_singly_linked_lists/101-main.c
@@ -36,29 +36,3 @@ int main(void)
     print_listint_safe(head);*/
     return (0);
 }
-
-size_t print_listint_safe(const listint_t *head)
-{
-	const listint_t *current;
-	const listint_t *runner;
-	size_t count = 0;
-
-	current = head;
-	while (current)
-	{
-		runner = head;
-		while (runner != current)
-		{
-			if (runner == current->next)
-			{
-				printf("[%p] %d\n", (void *)current, current->n);
-				return (count);
-			}
-			runner = runner->next;
-		}
-		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
-		count++;
-	}
-	return (count);
-}
