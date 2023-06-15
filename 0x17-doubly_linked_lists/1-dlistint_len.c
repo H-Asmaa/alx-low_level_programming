e710cb7caa4d4c32832324958a761cbdcb1602ef
H-Asmaa <asmaehadar32@gmail.com>
2023-06-15 17:15:07 +0100

    The length of a doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - check the code
+ * dlistint_len - check the code
  * @h: the head of the list
  * Return: size_t.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t nodes;
+
+	nodes = 0;
+	if (h != NULL)
+		while (h != NULL)
+		{
+			nodes++;
+			h = h->next;
+		}
+	return (nodes);
 }
