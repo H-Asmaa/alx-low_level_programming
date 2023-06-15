fba63e4e82dcc70ffeb70c0262fb5d605c84f1f0
H-Asmaa <asmaehadar32@gmail.com>
2023-06-15 20:00:07 +0100

    Adding an element at a given index in a doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,13 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *current, *prv;
 	unsigned int index = 0;
 
-	while ((*h)->prev != NULL)
-		*h = (*h)->prev;
-
 	if (idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
