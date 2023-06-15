04429fda24993bf6f2540756d8d09b202d8b8e9f
H-Asmaa <asmaehadar32@gmail.com>
2023-06-15 23:42:46 +0100

    Delete an item in a doubly liked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,35 +11,36 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int idx = 0;
 	dlistint_t *current, *p;
 
-	if (*head == NULL)
-		return (-1);
-	if (index == 0)
+	if (*head != NULL)
 	{
-		p = (*head);
-		if ((*head)->next != NULL)
+		if (index == 0)
 		{
-			(*head)->next->prev = NULL;
-			*head = (*head)->next;
+			p = (*head);
+			if ((*head)->next != NULL)
+			{
+				(*head)->next->prev = NULL;
+				*head = (*head)->next;
+				free(p);
+				return (1);
+			}
+			*head = NULL;
 			free(p);
 			return (1);
 		}
-		*head = NULL;
-		free(p);
-		return (1);
-	}
-	current = *head;
-	while (current != NULL)
-	{
-		if (index == idx)
+		current = *head;
+		while (current != NULL)
 		{
-			p = current;
-			current->prev->next = current->next;
-			current->next = current->prev;
-			free(p);
-			return (1);
+			if (index == idx)
+			{
+				p = current;
+				current->prev->next = current->next;
+				current->next->prev = current->prev;
+				free(p);
+				return (1);
+			}
+			current = current->next;
+			idx++;
 		}
-		current = current->next;
-		idx++;
 	}
 	return (-1);
 }
