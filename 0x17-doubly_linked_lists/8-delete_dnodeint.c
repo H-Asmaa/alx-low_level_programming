92aea7b7040c25e702422585a7a14bb325f5af81
H-Asmaa <asmaehadar32@gmail.com>
2023-06-16 00:01:39 +0100

    Correction

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,8 +32,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			if (index == idx)
 			{
-				current->prev->next = current->next;
-				current->next->prev = current->prev;
+				if (current->prev != NULL)
+					current->prev->next = current->next;
+				if (current->next != NULL)
+					current->next->prev = current->prev;
 				free(current);
 				return (1);
 			}
