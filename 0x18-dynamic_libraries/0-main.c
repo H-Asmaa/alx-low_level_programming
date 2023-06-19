eb42ea8cff15992de412799cc6a560a67431d083
H-Asmaa <asmaehadar32@gmail.com>
2023-06-19 14:52:03 +0100

    Leaving only .o files

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,28 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
-int _strlen(char *s)
-{
-	int i;
-	int count;
-
-	count = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		count++;
-	}
-
-	return (count);
-}
-
 int main(void)
 {
-	printf("%d\n", _strlen("My Dyn Lib"));
-	return (EXIT_SUCCESS);
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
 }
