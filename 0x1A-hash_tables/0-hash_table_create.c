b738e9795a9383e285047a5f29ad94caebbea3b6
H-Asmaa <asmaehadar32@gmail.com>
2023-07-08 00:41:50 +0100

    Committing hash table creation

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: variable
+ *
+ * Return: hash_table_t.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	int i;
+
+	if (size <= 0)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+	(void)ht;
+    return (EXIT_SUCCESS);
+}
