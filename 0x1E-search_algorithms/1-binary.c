088a17844900e55b8aad70a4dd63987c6650c9e5
H-Asmaa <asmaehadar32@gmail.com>
2023-10-10 11:10:44 +0100

    Committing a correctionof output

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 #include <stddef.h>
-
+#include <stdio.h>
 /**
  * binary_search - A function that does binary search
  * @array: A pointer to the first element of the array to search in.
@@ -10,7 +10,7 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t start_idx, end_idx, mid_idx;
+	size_t start_idx, end_idx, mid_idx, i;
 
 	start_idx = 0;
 	end_idx = size - 1;
@@ -19,6 +19,10 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 	while (start_idx <= end_idx)
 	{
+		printf("Searching in array: ");
+		for (i = start_idx; i < end_idx ; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[end_idx]);
 		mid_idx = (start_idx + end_idx) / 2;
 		if (array[mid_idx] == value)
 		{
